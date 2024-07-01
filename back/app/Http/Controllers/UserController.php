e5635f712ddca437d99334ffa5c28d748c4e5ff8
ibrahimbenmagha <ibrahimbenmagha16@gmail.com>
2024-07-01 19:14:05 +0200

    a

--- back/app/Http/Controllers/UserController.php
+++ back/app/Http/Controllers/UserController.php
@@ -10,28 +10,28 @@ use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
-    public function create_client(Request $request)
+    public function Create_client(Request $request)
     {
-        // Validation des données entrantes
         $request->validate([
             'name' => 'required|string|max:255',
             'email' => 'required|string|email|max:255|unique:users',
             'password' => 'required|string|min:8',
             'role' => 'required|string|in:client,artisan',
         ]);
-
-        // Création de l'utilisateur
         $user = new User();
         $user->name = $request->name;
         $user->email = $request->email;
         $user->password = Hash::make($request->password);
-        $user->role = $request->role;
+        $user->role = "admin"; //automatically added 
         $user->save();
-
-        // Retourner une réponse réussie avec l'utilisateur créé
         return response()->json([
             'message' => 'User created successfully',
             'user' => $user
         ], 201);
     }
+
+
+
+  
+
 }
