0a91ab7771b94e7c95a805f342f2c1b5f6aae58f
ibrahimbenmagha <ibrahimbenmagha16@gmail.com>
2024-07-01 17:47:05 +0200

    Corriger les migrations

---
+++ back/database/migrations/3_Commandes.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('commandes', function (Blueprint $table) {
+            $table->id();
+            $table->timestamp('service_date');
+            $table->float('total_price');
+            $table->enum('status',['pending', 'in progress', 'completed']);
+
+            $table->foreignId('client_id')->references('id')->on('clients');
+            $table->foreignId('artisan_id')->references('id')->on('artisans');
+            $table->foreignId('artisan_service_id')->references('id')->on('service_artisan');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('commandes');
+    }
+};
